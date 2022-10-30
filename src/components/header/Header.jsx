783c0844fe568855bc3016a7cb0700cfe1db92a8
zucolinz <collinsizuchukwu18@gmail.com>
2022-10-30 09:11:41 +0100

    header name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,10 +8,10 @@ const Header = () => {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleSm">ZucoBlog & ShowLife</span>
         <span className="headerTitleLg">Blog</span>
       </div>
-      <img className="headerImg" src={Heropic} alt="" />
+      <img className="headerImg" src={Heroic} alt="" />
     </div>
   );
 };
